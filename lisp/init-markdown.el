000d28b1fee2cadfbd3941c1b854e0159f38921a
Marcelo M. A <ma.munoz.araya@gmail.com>
2015-11-04 01:12:51 -0300

    update markdown-mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,3 +1,6 @@
 (require-package 'mkdown)
-(setq markdown-css-path mkdown-css-file-name)
+(setq markdown-content-type "text/html")
+(setq path (cons mkdown-css-file-name ()))
+(setq markdown-css-paths path)
+
 (provide 'init-markdown)
