d787162810c716b516cc37ad198a73c90ec0dea0
Marcelo M. A <ma.munoz.araya@gmail.com>
2016-08-08 01:39:04 -0400

    Remove things

--- defuns/misc-defuns.el
+++ defuns/misc-defuns.el
@@ -1,4 +1,4 @@
-;; Taken from magnars .emacs.d (https://github.com/magnars/.emacs.d.git)
+;; Something taken from magnars .emacs.d (https://github.com/magnars/.emacs.d.git)
 
 ;; Misc defuns go here
 ;; It wouldn't hurt to look for patterns and extract once in a while
@@ -76,74 +76,6 @@
 
 ;; (global-set-key (kbd "s-l") (λ (insert "\u03bb")))
 
-;; command to help set up magit-gh-pulls
-;; (defun magit-gh-pulls-setup (repoid)
-;;   (interactive "suser/repo: ")
-;;   (shell-command "git config --add magit.extension gh-pulls")
-;;   (shell-command (concat "git config magit.gh-pulls-repo " repoid)))
-
-;; Increase/decrease selective display
-(defun inc-selective-display (arg)
-  (interactive "P")
-  (if (numberp arg)
-      (set-selective-display arg)
-    (if (numberp selective-display)
-        (set-selective-display (+ 2 selective-display))
-      (set-selective-display 2)))
-  (create-temp-selective-display-keymap))
-
-(defun dec-selective-display ()
-  (interactive)
-  (when (and (numberp selective-display)
-             (> selective-display 2))
-    (set-selective-display (- selective-display 2)))
-  (create-temp-selective-display-keymap))
-
-(defun clear-selective-display ()
-  (interactive)
-  (when (numberp selective-display)
-    (set-selective-display nil)))
-
-(defun create-temp-selective-display-keymap ()
-  (set-temporary-overlay-map
-   (let ((map (make-sparse-keymap)))
-     (define-key map (kbd "+") 'inc-selective-display)
-     (define-key map (kbd "-") 'dec-selective-display)
-     (define-key map (kbd "0") 'clear-selective-display)
-     map))
-  (message "Type + to reveal more, - for less, 0 to reset."))
-
-
-(defun isearch-yank-selection ()
-  "Put selection from buffer into search string."
-  (interactive)
-  (when (region-active-p)
-    (deactivate-mark))
-  (isearch-yank-internal (lambda () (mark))))
-
-(defun region-as-string ()
-  (buffer-substring (region-beginning)
-                    (region-end)))
-
-(defun isearch-forward-use-region ()
-  (interactive)
-  (when (region-active-p)
-    (add-to-history 'search-ring (region-as-string))
-    (deactivate-mark))
-  (call-interactively 'isearch-forward))
-
-(defun isearch-backward-use-region ()
-  (interactive)
-  (when (region-active-p)
-    (add-to-history 'search-ring (region-as-string))
-    (deactivate-mark))
-  (call-interactively 'isearch-backward))
-
-(eval-after-load "multiple-cursors"
-  '(progn
-     (unsupported-cmd isearch-forward-use-region ".")
-     (unsupported-cmd isearch-backward-use-region ".")))
-
 
 (defun buffer-to-html (buffer)
   (with-current-buffer (htmlize-buffer buffer)
@@ -154,30 +86,3 @@
   (if (or (not (eql  arg 1)) (not buffer-file-name))
       (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
-
-
-(defun add-file-find-hook-with-pattern (pattern fn &optional contents)
-  "Add a find-file-hook that calls FN for files where PATTERN
-matches the file name, and optionally, where CONTENT matches file contents.
-Both PATTERN and CONTENTS are matched as regular expressions."
-  (lexical-let ((re-pattern pattern)
-                (fun fn)
-                (re-content contents))
-    (add-hook 'find-file-hook
-              (lambda ()
-                (if (and
-                     (string-match re-pattern (buffer-file-name))
-                     (or (null re-content)
-                         (string-match re-content
-                                       (buffer-substring (point-min) (point-max)))))
-                    (apply fun ()))))))
-
-;; Fix kmacro-edit-lossage, it's normal implementation
-;; is bound tightly to C-h
-(defun kmacro-edit-lossage ()
-  "Edit most recent 300 keystrokes as a keyboard macro."
-  (interactive)
-  (kmacro-push-ring)
-  (edit-kbd-macro 'view-lossage))
-
-(defmacro comment (&rest ignore))
