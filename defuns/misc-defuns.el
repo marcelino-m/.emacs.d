3ceb6edbb83f5ebcde59d15559989e733a1b33d5
Marcelo M. A <ma.munoz.araya@gmail.com>
2016-09-09 17:39:12 -0300

    Add function to create emptu buffer

--- defuns/misc-defuns.el
+++ defuns/misc-defuns.el
@@ -134,3 +134,14 @@ prefix argument keep point in curretn position"
     (ispell-change-dictionary change)
     (message "Dictionary switched from %s to %s" dic change)
     ))
+
+;; Create a new empy buffer
+(defun ma/new-empty-buffer ()
+  "Open a new empty buffer.
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2016-08-11"
+  (interactive)
+  (let ((-buf (generate-new-buffer "untitled")))
+    (switch-to-buffer -buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)))
