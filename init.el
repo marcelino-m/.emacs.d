6e6efe37e47bcb82b4aabd1d3577c1e72977a8f1
Marcelo M. A <ma.munoz.araya@gmail.com>
2016-10-01 20:53:12 -0300

    Add tide mode

--- init.el
+++ init.el
@@ -712,8 +712,28 @@
   (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
   )
 
-;; (require 'init-dired)
-;; (require 'init-translate)
+
+(use-package tide
+  :ensure t
+  :mode ("\\.ts\\'" . typescript-mode)
+  :init
+  (defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1))
+
+  :config
+  ;; aligns annotation to the right hand side
+  (setq company-tooltip-align-annotations t)
+  (add-hook 'typescript-mode-hook #'setup-tide-mode))
+
+
+
+
 ;; (require 'init-zeal)
-;; (require 'init-c)
-;; (require 'init-ts)
