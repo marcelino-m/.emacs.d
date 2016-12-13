3ca4be55c24ab7882e6430717cc57334c0c93646
Marcelo M. A <ma.munoz.araya@gmail.com>
2016-12-13 03:09:20 -0300

    clean yas

--- init.el
+++ init.el
@@ -156,30 +156,10 @@
          ("C-c y n"   . yas-new-snippet)
          ("C-c y v"   . yas-visit-snippet-file)
          )
-  :preface
-  (defun yas-new-snippet (&optional choose-instead-of-guess)
-    (interactive "P")
-    (let ((guessed-directories (yas-guess-snippet-directories)))
-      (switch-to-buffer "*new snippet*")
-      (erase-buffer)
-      (kill-all-local-variables)
-      (snippet-mode)
-      (set (make-local-variable 'yas-guessed-modes)
-           (mapcar #'(lambda (d) (intern (yas-table-name (car d))))
-                   guessed-directories))
-      (unless (and choose-instead-of-guess
-                   (not (y-or-n-p "Insert a snippet with useful headers? ")))
-        (yas-expand-snippet
-         (concat "\n"
-                 "# -*- mode: snippet -*-\n"
-                 "# name: $1\n"
-                 "# --\n"
-                 "$0\n")))))
-
   :config
   (yas-load-directory "~/.emacs.d/snippets/")
-  (yas-global-mode 1))
-
+  (yas-global-mode 1)
+  )
 
 (use-package prog-mode
   :defer t
