dc148874c3b422466ebbe61d0c724e8192e45e76
Marcelo M. A <marcelo.munoz@timining.cl>
2017-01-04 16:30:47 -0300

    Actualize build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# initialize and update submodules
+git submodule update --init --recursive
+
 # system dependecies for ycmd
 echo -e "${RED}Installing:${NC} dependencies for ycmd"
 sudo aptitude install python-dev python3-dev build-essential cmake
@@ -9,10 +12,8 @@ sudo aptitude install python-dev python3-dev build-essential cmake
 # dictionaries
 echo -e "${RED}Installing:${NC} dictionaries"
 sudo aptitude install aspell-es aspell-en
-
-# misc
-echo -e "${RED}Installing:${NC} sqlitebrowser"
-sudo aptitude install sqlitebrowser
+echo -e "${RED}Installing:${NC} Ag silver search"
+sudo aptitude install  silversearcher-ag
 
 HOME_USER=/home/$USER
 
