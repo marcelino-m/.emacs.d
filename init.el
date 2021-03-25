61e995dcdf9875229481adc4f0a494ebbbb2e1cb
Marcelo M. A <ma.munoz.araya@gmail.com>
2021-03-25 13:14:03 -0300

    Remove unused var

--- init.el
+++ init.el
@@ -254,9 +254,6 @@ NAME can be used to set the name of the defined function."
               (split-string
                (shell-command-to-string "find ~/syncthing/org/ -type f -name \"*.org\"") "\n")))
 
-  (setq ma/work-org-agenda-files
-        (seq-filter (lambda (s) (string-match-p "/work/" s)) org-agenda-files))
-
   (setq org-agenda-custom-commands
         '(("A" "Personal agenda for current day or week" agenda ""
            ((org-agenda-tag-filter-preset '("+@personal"))))
