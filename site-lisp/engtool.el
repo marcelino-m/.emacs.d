41582624db1f0da6c38b8a3b44f5bf06e18c6e68
Marcelo M. A <ma.munoz.araya@gmail.com>
2021-05-22 21:35:22 -0400

    engtool-mode learn to play specific part of media file

--- site-lisp/engtool.el
+++ site-lisp/engtool.el
@@ -1,8 +1,13 @@
+;;; engtool.el-*- lexical-binding: t; -*-
 
+(require 'org-timer)
 
 (defcustom engtool-irregular-verbs-file "~/lab/irregular/list-irregular-verbs.csv"
   "Path to file with irregular verbs")
 
+(defvar engtool-last-used-file-path nil)
+(defvar engtool-last-used-file-start nil)
+(defvar engtool-last-used-file-end nil)
 
 (defun engtool--pronounce-url-from-oxford-dict (word)
   (let* ((baseurl "https://www.oxfordlearnersdictionaries.com/media/english/us_pron")
@@ -23,6 +28,31 @@
   (concat "https://www.oxfordlearnersdictionaries.com/definition/english/" word))
 
 
+(defun engtool--build-link-to-segment-file (path start end)
+  (cond
+   ((and start end)
+    (insert (format "[[say:%s::%s--%s][<%s %s>]]" path start end start end)))
+   (start
+    (insert (format "[[say:%s::%s--][<%s end>]]" path start start)))
+   (end
+    (insert (format "[[say:%s::--%s][<start %s>]]" path end end)))
+   (t
+    (insert (format "[[say:%s][<all>]]" path)))))
+
+
+
+(defun engtool--play-with-mpv-shell-commnad (file start end)
+  (if (file-exists-p file)
+      (let ((spos "")
+            (epos  ""))
+        (when start
+          (setq spos (format " --start=%s " start)))
+        (when end
+          (setq epos (format " --end=%s " end)))
+        (start-process-shell-command "mpv" nil (concat "mpv " file spos epos)))
+    (message "file not found: %s" path)))
+
+
 (defun engtool-pronounce-url (word)
   (engtool--pronounce-url-from-oxford-dict (downcase word)))
 
@@ -41,7 +71,7 @@
     (completing-read "Irregular verb: " verb)))
 
 
-(defun engtool-linkify-word-dwim (word)
+(defun engtool-linkify-word-atpoint-to-sound-dict (word)
   (interactive (let ((word (bounds-of-thing-at-point 'word)))
                  (if word
                      (list word)
@@ -52,13 +82,66 @@
   (insert (format "[[say:%s][%s]]" (engtool-pronounce-url word) word)))
 
 
-(defun engtool-play-local-file (path)
+(defun engtool-unlikify-atpoint ()
+    "Replace an org link by its description or if empty its address"
+  (interactive)
+  (if (org-in-regexp org-link-bracket-re 1)
+      (save-excursion
+        (let ((remove (list (match-beginning 0) (match-end 0)))
+              (description
+               (if (match-end 2)
+                   (org-match-string-no-properties 2)
+                 (org-match-string-no-properties 1))))
+          (apply 'delete-region remove)
+          (insert description)))))
+
+
+(defun engtool-insert-link-to-file-within-segment (path start end)
+  (interactive (let ((path engtool-last-used-file-path))
+                 (list
+                  (read-file-name "select a file: " nil nil t path)
+                  (org-timer-fix-incomplete
+                   (read-string "start in (h:mm:ss): " engtool-last-used-file-end))
+                  (org-timer-fix-incomplete
+                   (read-string "end at (h:mm:ss): " )))))
+  (setq engtool-last-used-file-path  (expand-file-name path)
+        engtool-last-used-file-start start
+        engtool-last-used-file-end   end)
+
+  (insert (engtool--build-link-to-segment-file
+           engtool-last-used-file-path
+           engtool-last-used-file-start
+           engtool-last-used-file-end)))
+
+
+(defun engtool-parse-path (filepath)
+  "[http[s]://]/path/tofile[::[h:mm:ss]--[h:mm:ss]]"
+
+  (let* ((parts (split-string filepath "::" t))
+         (path    (nth 0 parts))
+         (segment (split-string (or (nth 1 parts) "--") "--"))
+         (start   (nth 0 segment))
+         (end     (nth 1 segment)))
+    (list
+     start end
+     :path path
+     :start (unless (string-empty-p start)
+              start)
+     :end (unless (string-empty-p end)
+            end))))
+
+
+(defun engtool-play-local-file (path start end)
   (if (file-exists-p path)
-      (start-process-shell-command "mplayer" nil (concat "mplayer " path))
+      (engtool-play-file path start end)
     (message "file not found: %s" path)))
 
 
-(defun engtool-play-remote-file (url)
+(defun engtool-play-file (file start end)
+  (engtool--play-with-mpv-shell-commnad file start end))
+
+
+(defun engtool-play-remote-file (url start end  &optional succfn errfn)
   (request  url
     :encoding 'binary
     :success (cl-function
@@ -67,15 +150,14 @@
                   (with-temp-file tmpfile
                     (set-buffer-file-coding-system 'raw-text)
                     (insert data))
-                  (start-process-shell-command "mplayer" nil (concat "mplayer " tmpfile)))))
-
+                  (engtool-play-file tmpfile start end))))
 
     :error (cl-function
             (lambda (&key response &allow-other-keys)
               (let ((code (request-response-status-code response)))
-                (if (= 404 code)
-                    (message "Word wasn't found")
-                  (message "An expected error occur getting the word")))))))
+                (if errfn
+                    (funcall errfn code)
+                  (message "url wasn't found")))))))
 
 
 (defun engtool-pronounce-this-word (word)
@@ -85,7 +167,13 @@
                         (set-text-properties 0 (length word) nil word)
                         (list (read-string "Word: " word)))
                    (list (read-string "Word: ")))))
-  (engtool-play-remote-file (engtool-pronounce-url word)))
+
+  (let ((url (engtool-pronounce-url word)))
+    (engtool-play-remote-file
+     url nil nil nil (lambda (code)
+                   (if (= code 404)
+                       (message "Word: %s, wasn't found" word)
+                     (message "An expected error occur getting the word: http code: %d" code))))))
 
 
 (defun engtool-goto-definition-this-word (word)
