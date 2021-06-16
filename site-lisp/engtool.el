a805310912579ccd9e192f4253c766bc8bea1c9a
Marcelo M. A <ma.munoz.araya@gmail.com>
2021-06-16 08:59:20 -0400

    Improvements in engtool

--- site-lisp/engtool.el
+++ site-lisp/engtool.el
@@ -1,13 +1,18 @@
 ;;; engtool.el-*- lexical-binding: t; -*-
 
-(require 'org-timer)
+(require 'org)
+(require 'mpv)
+
 
 (defcustom engtool-irregular-verbs-file "~/lab/irregular/list-irregular-verbs.csv"
   "Path to file with irregular verbs")
 
-(defvar engtool-last-used-file-path nil)
-(defvar engtool-last-used-file-start nil)
-(defvar engtool-last-used-file-end nil)
+(defvar engtool-mpv-last-used-file-path nil)
+(defvar engtool-mpv-mark-queue nil)
+(defvar engtool-mpv-mark-queue-max-size 2)
+
+(setq mpv-default-options
+      (list "--audio-display=no"))
 
 (defun engtool--pronounce-url-from-oxford-dict (word)
   (let* ((baseurl "https://www.oxfordlearnersdictionaries.com/media/english/us_pron")
@@ -31,14 +36,13 @@
 (defun engtool--build-link-to-segment-file (path start end)
   (cond
    ((and start end)
-    (insert (format "[[say:%s::%s--%s][<%s %s>]]" path start end start end)))
+    (format "[[say:%s::%s--%s][<%s %s>]]" path start end start end))
    (start
-    (insert (format "[[say:%s::%s--][<%s end>]]" path start start)))
+    (format "[[say:%s::%s--][<%s END>]]" path start start))
    (end
-    (insert (format "[[say:%s::--%s][<start %s>]]" path end end)))
+    (format "[[say:%s::--%s][<START %s>]]" path end end))
    (t
-    (insert (format "[[say:%s][<all>]]" path)))))
-
+    (format "[[say:%s][<all>]]" path))))
 
 
 (defun engtool--play-with-mpv-shell-commnad (file start end)
@@ -53,6 +57,17 @@
     (message "file not found: %s" path)))
 
 
+(defun engtool--play-with-mpv-process (file start end)
+  (cond
+   ((and start end)
+    (mpv-start file (format "--start=%s" start) (format "--end=%s" end)))
+   (start
+    (mpv-start file (format "--start=%s" start)))
+   (end
+    (mpv-start file (format "--end=%s" end)))
+   (t
+    (mpv-start file))))
+
 (defun engtool-pronounce-url (word)
   (engtool--pronounce-url-from-oxford-dict (downcase word)))
 
@@ -64,10 +79,11 @@
 (defun engtool-irregular-verb-list ()
   (interactive)
   (let (verbs)
-    (setq verbs (split-string (with-temp-buffer
-                               (insert-file-contents-literally engtool-irregular-verbs-file)
-                               (buffer-string))
-                             "\n" t))
+    (setq verbs (split-string
+                 (with-temp-buffer
+                   (insert-file-contents-literally engtool-irregular-verbs-file)
+                   (buffer-string))
+                 "\n" t))
     (completing-read "Irregular verb: " verb)))
 
 
@@ -97,22 +113,50 @@
 
 
 (defun engtool-insert-link-to-file-within-segment (path start end)
-  (interactive (let ((path engtool-last-used-file-path))
-                 (list
-                  (read-file-name "select a file: " nil nil t path)
-                  (org-timer-fix-incomplete
-                   (read-string "start in (h:mm:ss): " engtool-last-used-file-end))
-                  (org-timer-fix-incomplete
-                   (read-string "end at (h:mm:ss): " )))))
-  (setq engtool-last-used-file-path  (expand-file-name path)
-        engtool-last-used-file-start start
-        engtool-last-used-file-end   end)
+  (interactive
+   (let* ((path   engtool-mpv-last-used-file-path)
+          (queue  engtool-mpv-mark-queue)
+          (start  (if (length>  queue 1)
+                      (cadr queue)
+                    (car queue)))
+          (end    (when (length> queue 1)
+                    (car queue))))
+     (list
+      (read-file-name "select a file: " nil nil t path)
+      (read-string "start in (h:mm:ss): " (when start
+                                            (org-timer-secs-to-hms (round start))))
+      (read-string "end at (h:mm:ss): " (when end
+                                          (org-timer-secs-to-hms (round end)))))))
 
-  (insert (engtool--build-link-to-segment-file
-           engtool-last-used-file-path
-           engtool-last-used-file-start
-           engtool-last-used-file-end)))
+  (if (string-empty-p start)
+      (setq start nil)
+    (setq start (org-timer-fix-incomplete start)))
 
+  (if (string-empty-p end)
+      (setq end nil)
+    (setq end (org-timer-fix-incomplete end)))
+
+  (setq engtool-mpv-mark-queue (nbutlast engtool-mpv-mark-queue))
+  (insert (engtool--build-link-to-segment-file path start end)))
+
+(defun engtool-mpv-last-start ()
+  (let ((queue engtool-mpv-mark-queue))
+    (if (length>  queue 1)
+        (cadr queue)
+      (car queue))))
+
+(defun engtool-mpv-last-end ()
+  (let ((queue engtool-mpv-mark-queue))
+    (when (length>  queue 1)
+      (car queue))))
+
+(defun engtool-play-from-last ()
+  (interactive)
+  (if (and engtool-mpv-last-used-file-path (file-exists-p engtool-mpv-last-used-file-path))
+      (engtool-play-file engtool-mpv-last-used-file-path
+                         (engtool-mpv-last-start)
+                         (engtool-mpv-last-end))
+    (message "no previus file was played")))
 
 (defun engtool-parse-path (filepath)
   "[http[s]://]/path/tofile[::[h:mm:ss]--[h:mm:ss]]"
@@ -131,17 +175,30 @@
             end))))
 
 
-(defun engtool-play-local-file (path start end)
+(defun engtool-select-and-play-file (path start end)
+  (interactive (list
+                (read-file-name "select a file: " nil nil t)
+                (let ((start (read-string "start in (h:mm:ss): ")))
+                  (unless (string-empty-p start)
+                    (org-timer-fix-incomplete start)))
+                (let ((end (read-string "end at (h:mm:ss): ")))
+                  (unless (string-empty-p end)
+                    (org-timer-fix-incomplete end)))))
+
   (if (file-exists-p path)
-      (engtool-play-file path start end)
+      (progn
+        (engtool-play-file path start end)
+        (setq engtool-mpv-last-used-file-path path))
     (message "file not found: %s" path)))
 
 
 (defun engtool-play-file (file start end)
-  (engtool--play-with-mpv-shell-commnad file start end))
+   (if (file-exists-p file)
+       (engtool--play-with-mpv-process file start end)
+     (error "file not found: %s" file)))
 
 
-(defun engtool-play-remote-file (url start end  &optional succfn errfn)
+(defun engtool-play-url (url start end  &optional succfn errfn)
   (request  url
     :encoding 'binary
     :success (cl-function
@@ -169,7 +226,7 @@
                    (list (read-string "Word: ")))))
 
   (let ((url (engtool-pronounce-url word)))
-    (engtool-play-remote-file
+    (engtool-play-url
      url nil nil nil (lambda (code)
                    (if (= code 404)
                        (message "Word: %s, wasn't found" word)
@@ -203,14 +260,74 @@
     (when-let ((word (thing-at-point 'word)))
         (engtool-goto-definition-this-word word))))
 
+(defvar engtool-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "S-<mouse-1>") 'engtool-pronounce-this-word-on-click)
+    (define-key map (kbd "S-<mouse-3>") 'engtool-goto-definition-this-on-click)
+    map))
+
+
+(defun engtool-follow-link(fpath _)
+  (let* ((pp    (engtool-parse-path fpath))
+         (path  (plist-get pp :path))
+         (start (plist-get pp :start))
+         (end   (plist-get pp :end)))
+    (if (or (string-prefix-p "http://" path)
+            (string-prefix-p "https://" path))
+        (engtool-play-url path start end)
+      (engtool-play-file path start end))))
+
+(org-link-set-parameters "say" :follow #'engtool-follow-link)
+
 
 (define-minor-mode engtool-mode
   "Function collection to help english learning"
-  :lighter " engtool "
+  :lighter " engtool"
   :global t
-  :keymap (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "S-<mouse-1>") 'engtool-pronounce-this-word-on-click)
-            (define-key map (kbd "S-<mouse-3>") 'engtool-goto-definition-this-on-click)
-            map))
+  :keymap engtool-mode-map)
+
+
+(defun engtool-mpv-mark-position ()
+  (interactive)
+  (if-let ((pos    (mpv-get-playback-position))
+           (qmax   engtool-mpv-mark-queue-max-size))
+      (progn
+        (setq engtool-mpv-mark-queue (cons pos engtool-mpv-mark-queue))
+        (when (length>  engtool-mpv-mark-queue qmax)
+          (setq engtool-mpv-mark-queue (nbutlast engtool-mpv-mark-queue)))
+        (message "mark: %s" (org-timer-fix-incomplete
+                             (number-to-string (round pos)))))
+    (message "nothing currently playing")))
+
+
+
+(defvar engtool-mpv-command-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "SPC") 'mpv-pause)
+    (define-key map (kbd "q")   'mpv-kill)
+    (define-key map (kbd "f")   'mpv-seek-forward)
+    (define-key map (kbd "b")   'mpv-seek-backward)
+    map))
+
+
+(defvar engtool-mpv-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c m") engtool-mpv-command-map)
+    map))
+
+(define-minor-mode engtool-mpv-mode
+  "Custom key binding while play file"
+  :lighter " mpv"
+  :keymap engtool-mpv-mode-map)
+
+(defun engtool-mpv-enable-on-start-hook (arg)
+  (engtool-mpv-mode 1))
+
+(defun engtool-mpv-disable-on-exit-hook ()
+  (engtool-mpv-mode -1))
+
+
+(add-hook 'mpv-on-start-hook 'engtool-mpv-enable-on-start-hook)
+(add-hook 'mpv-on-exit-hook  'engtool-mpv-disable-on-exit-hook)
 
 (provide 'engtool)
