2a55c07b4856baaba9e75cbb6e791559010318d7
Marcelo M. A <ma.munoz.araya@gmail.com>
2026-01-02 14:04:45 -0300

    Saving

--- init.el
+++ init.el
@@ -638,6 +638,11 @@ taken from: https://emacsredux.com/blog/2025/06/01/let-s-make-keyboard-quit-smar
 
   (projectile-global-mode))
 
+(use-package wgrep
+  :ensure t
+  :diminish)
+
+
 (use-package ispell
   :custom
   (ispell-program-name  "hunspell")
@@ -746,7 +751,9 @@ taken from: https://emacsredux.com/blog/2025/06/01/let-s-make-keyboard-quit-smar
 
 (use-package forge
   :ensure t
-  :after magit)
+  :after magit
+  :custom-face
+  (forge-pullreq-draft ((t (:inherit default :background nil :foreground "gray63" :bold t)))))
 
 (use-package git-modes
   :ensure t)
@@ -842,7 +849,8 @@ taken from: https://emacsredux.com/blog/2025/06/01/let-s-make-keyboard-quit-smar
 
   ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
   :bind (:map corfu-mode-map
-              ("C-M-s-c" . completion-at-point))
+              ("C-M-s-c" . completion-at-point)
+              ("M-c" .  corfu-quick-complete))
 
   :hook ((prog-mode      . corfu-mode)
          (shell-mode     . corfu-mode)
@@ -851,13 +859,55 @@ taken from: https://emacsredux.com/blog/2025/06/01/let-s-make-keyboard-quit-smar
          (python-ts-mode . corfu-mode))
 
   :init
-
   ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
   ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
   ;; variable `global-corfu-modes' to exclude certain modes.
   (global-corfu-mode)
   (corfu-indexed-mode)
   (corfu-popupinfo-mode))
+
+
+(use-package embark
+  :ensure t
+  :bind
+  (("s-t" . embark-act)         ;; pick some comfortable binding
+   ;; ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
+  ;; (context-menu-mode 1)
+  ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
+
+
+
 
 ;; ;; A few more useful configurations...
 ;; (use-package emacs
